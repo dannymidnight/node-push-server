67ae8a54f897cace1d5658702f77ce73187ae625
Chris Campbell <chris.campbell@99designs.com>
2012-06-29 15:14:16 +1000

    Add production configuration

--- pocket.js
+++ pocket.js
@@ -6,6 +6,15 @@ var config = require('./config'),
     client = require('./client'),
     server = require('./server');
 
+server.configure('production', function(){
+  server.set('log level', 1);
+  server.set('transports', ['websocket']);
+});
+
+server.configure('development', function(){
+  server.set('transports', ['websocket']);
+});
+
 client.on("notification", function(data) {
   server.emit("notification", data.userid, data.data);
 });
