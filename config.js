28858f84553f31ec271a13636ec6cd8999036cdf
Chris Campbell <chris.campbell@99designs.com>
2012-06-29 11:52:32 +1000

    Add optional local configuration

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
+_ = require('underscore');
+
+try {
+  var locals = require('./locals');
+} catch (e) {
+  locals = {};
+  console.log('WARN: Missing locals config file');
+}
+
+// Defaults
 var config = {
 	udp_port: 4000,
 	web_port: 8080,
 	secretkey: 'pocket'
 };
 
-module.exports = config;
+module.exports = _.extend(config, locals);
