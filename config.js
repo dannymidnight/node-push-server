bd42ea12624d7f4604c87d95054ca20b29645146
Paul Leitmanis <paul.leitmanis@99designs.com>
2012-08-20 22:29:33 +1000

    Check for valid user session with main http server

--- config.js
+++ config.js
@@ -11,7 +11,8 @@ try {
 var config = {
 	udp_port: 4000,
 	web_port: 8080,
-	secretkey: 'pocket'
+	secretkey: 'pocket',
+	contests_http_host: '99designs.com'
 };
 
 module.exports = _.extend(config, locals);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var	sha1   	= require('sha1'),
     winston	= require('winston'),
     util		= require('util'),
     config 	= require('./config'),
+		http		= require('http'),
     io     	= require('socket.io').listen(config.web_port);
 
 var serverlog = winston.loggers.get('server');
@@ -16,15 +17,34 @@ io.sockets.on('connection', function(socket) {
 	var user;
 
 	socket.on('user', function(data) {
-		var hash = sha1(data.id + config.secretkey);
 
-		if (data.hash === hash) {
-			// Register socket for user.
-			user = { id: data.id };
-      socket.join(user.id);
+		serverlog.info(data.sessionid);
 
-      serverlog.info(util.format('[server] User #%d connected via %s', data.id, io.transports[socket.id].name));
-		}
+		var options = {
+			host: config.contests_http_host,
+			port: 80,
+			path: '/users/sessioncheck/' + data.userid + "/" + data.sessionid
+		};
+
+		var req = http.request(options, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (http_response) {
+				serverlog.info("http_response = " + http_response);
+				if (http_response) {
+					// Register socket for user.
+					user = { id: data.userid };
+					socket.join(user.id);
+
+					serverlog.info(util.format('[server] User #%d connected via %s', data.userid, io.transports[socket.id].name));
+				}
+			});
+		});
+
+		req.on('error', function(e) {
+			console.log('problem with request: ' + e.message);
+		});
+
+		req.end();
 	});
 
 	socket.on('seen', function(data) {
