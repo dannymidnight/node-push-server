d38310f1a55bb8aa33e8d239c7a60fecafb8729a
Chris Campbell <chris@harto.org>
2012-09-24 12:16:19 +1000

    Add locals template file

---
+++ locals.defaults.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  web_port: 3000,
+  contests_http_host: '99designs.com.vm',
+
+  redis: {
+    port: '6379',
+    host: '127.0.0.1',
+    auth: '' // Assumes no redis requirepass is blank
+  }
+};
